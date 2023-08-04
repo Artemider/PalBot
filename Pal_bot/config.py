a7081903808d4428dac6dd19636235c44af35efd
Artemider <99029606+Artemider@users.noreply.github.com>
2023-08-04 13:27:54 +0300

    Update config.py

--- Pal_bot/config.py
+++ Pal_bot/config.py
@@ -1,15 +1,15 @@
-TOKEN = '5836308814:AAE5_msiYoHPVTDLSMQZzPzgLdEpd4OT81o'
+TOKEN = ''
 
 #TOKEN = '5431759596:AAGK8h3J6p7a8RF-vueVg-lefOCL2xGsJUg'
 
 base = 'basedata.db'
 
-owner = ["1101984099"]
+owner = [""]
 
-admins = ["1101984099"]
+admins = [""]
 
-moderator = ["1101984099"]
+moderator = [""]
 
-VIP = ["1101984099", "1667849940", "1832789743"]
+VIP = ["", "", ""]
 
 ignore = []
